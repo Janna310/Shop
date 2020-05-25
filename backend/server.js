e7efbcc3cca6e88577199500beddfae80a618f57
Janna Lodwick <57925340+Janna310@users.noreply.github.com>
2020-05-25 14:52:04 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ const app = express();
 const routes = require("./routes");
 const port = 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "../frontend/public"));
 app.use(express.json());
 
-app.use("/api", routes);
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Server is running: ${port}`);
